7c0ddecb8dd82d45c86fd50bc6b132c09b3bb886
peerlessblue <peerless.blue@live.com>
2023-03-13 20:07:57 -0500

    graph improved, further documentation

--- tiktok.py
+++ tiktok.py
@@ -49,12 +49,26 @@ def fools_game(*_):
 	return list_game(lambda x,_: random.choice(x))
 
 def smart_game(*_):
+	"""equivalent code:
+		def choice(valid_indices, num):
+			num_range = 1000
+			num_indices = 20
+			width = num_range / num_indices
+			best_choice = valid_indices[0]
+			best_distance = abs((valid_indices[0] + 0.5) * width - num)
+			for x in valid_indices:
+				distance = abs((x + 0.5) * width - num)
+				if distance < best_distance:
+					best_choice = x
+					best_distance = distance
+			return best_choice
+	"""
 	return list_game(lambda x,y: min(x, key=lambda z:abs((z+0.5)*50-y)))
 
 def main():
 	random_results = []
 	solid_results = []
-	trials = 100000
+	trials = 1000000
 	multipro = True
 
 	#run trials for both options, time results
@@ -77,19 +91,27 @@ def main():
 
 	#plot results using matplotlib
 	fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
-	for ax in axs: ax.set_xticks(range(20))
-	axs[0].hist(random_results, bins = len(random_counter))
-	axs[1].hist(solid_results, bins = len(solid_counter))
+	for ax in axs: ax.set_xticks(range(21))
+	bars_0 = axs[0].bar(random_counter.keys(), random_counter.values())
+	bars_1 = axs[1].bar(solid_counter.keys(), solid_counter.values())
+	axs[0].set_title('Random Valid Placements', fontstyle='italic')
+	axs[1].set_title('Nearest Available to Predicted Index', fontstyle='italic')
+	axs[0].set_xlabel("Numbers successfully added")
+	axs[1].set_xlabel("Numbers successfully added")
+	axs[0].bar_label(bars_0, [f"{x:.0f}: {y:.0f}" for x,y in zip(random_counter.keys(), random_counter.values())], fontsize=12, color='black')
+	axs[1].bar_label(bars_1, [f"{x:.0f}: {y:.0f}" for x,y in zip(solid_counter.keys(), solid_counter.values())], fontsize=12, color='black')
+	plt.ylabel("Trials observed")
 	plt.show()
 
 if __name__ == "__main__":
 	main()
 
-# initial:  100000 trials done in 211.39 ns per iteration
-# mp:       100000 trials done in 160.98 ns per iteration
-# mp tuple: 100000 trials done in 139.34 ns per iteration
-# mp chunk: 100000 trials done in 52.62 ns per iteration
-# rm comp:  100000 trials done in 45.21 ns per iteration
-# new adj:  100000 trials done in 33.08 ns per iteration
+# initial:	100000 trials done in 211.39 ns per iteration
+# mp:		100000 trials done in 160.98 ns per iteration
+# mp tuple:	100000 trials done in 139.34 ns per iteration
+# mp chunk:	100000 trials done in 52.62 ns per iteration
+# rm comp:	100000 trials done in 45.21 ns per iteration
+# new adj:	100000 trials done in 33.08 ns per iteration
 # rewrite:	100000 trials done in 28.97 ns per iteration
-# w/ multi: 100000 trials done in 17.82 ns per iteration
+# w/ multi:	100000 trials done in 17.82 ns per iteration
+# batch +:	1000000 trials done in 5.90 ns per iteration
